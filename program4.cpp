4ea2c640e2c6c77335fa2080e557516c6c39c90a
kgkinjalgupta <guptakinjal2005@gmail.com>
2026-05-04 14:54:49 +0530

    commit 1a2b3c4d5e6f7g8h9i0j Author: kgkinjalgupta<kgkinjalgupta@gmail.com>

---
+++ program4.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter a number:";
+    cin>>n;
+    for (int i=1;i<=n; i++){
+        for (int j =1; j<=(n-1); j++)
+        cout<<" ";
+        }
+    for (int k=1; k<=j; k++)
+cout<<"*";
+    }
+    cout<<endl;
+    return 0;
+}
